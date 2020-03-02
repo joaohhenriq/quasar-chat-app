a576f77e74a8b844151f02a6752615bc2b935e69
joaoh <joao_camargo10@hotmail.com>
2020-03-02 20:46:28 -0300

    Users list

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,11 +1,47 @@
 <template>
-  <q-page class="flex">
-    <h5>Users page</h5>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" separator>
+      <q-item v-for="user in users" :key="user.id" clickable v-ripple to="/chat">
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">{{ user.name.charAt(0) }}</q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge
+            :color="user.online ? 'light-green-5' : 'grey-4'"
+          >{{ user.online ? 'Online' : 'False' }}</q-badge>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      users: [{
+        id: 1,
+        name: 'Ruddy Jedrzej',
+        online: true
+      }, {
+        id: 2,
+        name: 'Mallorie Alessandrini',
+        online: false
+      }, {
+        id: 3,
+        name: 'Elisabetta Wicklen',
+        online: true
+      }, {
+        id: 4,
+        name: 'Seka Fawdrey',
+        online: false
+      }]
+    }
+  }
 }
 </script>
