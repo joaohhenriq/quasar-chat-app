d10d987b1d5c8d8da489900845a2586612c6fed9
joaoh <joao_camargo10@hotmail.com>
2020-03-03 21:04:45 -0300

    Auth layout

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-form @submit="submitForm">
+    <q-input
+      v-if="tab === 'register'"
+      class="q-mb-md"
+      outlined
+      v-model="formData.name"
+      label="Name"
+    />
+    <q-input class="q-mb-md col" outlined v-model="formData.email" label="Email" />
+    <q-input
+      class="q-mb-md col"
+      outlined
+      v-model="formData.password"
+      label="Password"
+      type="password"
+    />
+    <div class="row justify-center">
+      <q-btn color="primary" :label="tab" type="submit" />
+    </div>
+  </q-form>
+</template>
+
+<script>
+export default {
+  props: ['tab'],
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      if (this.tab === 'login') {
+        console.log('login');
+      } else {
+        console.log('register');
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,11 +1,50 @@
 <template>
-  <q-page class="flex">
-    <h5>Auth page</h5>
+  <q-page class="q-px-md row justify-center items-center">
+    <q-card class="auth-tabs">
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Login" />
+        <q-tab name="register" label="Register" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login">
+          <login-register :tab="tab" />
+        </q-tab-panel>
+
+        <q-tab-panel name="register">
+          <login-register :tab="tab" />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tab: 'login'
+    }
+  },
+  components: {
+    'login-register': require('components/LoginRegister.vue').default
+  }
 }
 </script>
+
+<style>
+.auth-tabs {
+  width: 500px;
+  margin: 0 auto;
+}
+</style>
